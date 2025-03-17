87bd7c26ad4a6a995d424d778c35ad4a593acfea
Sabadon <fedonenkodaniL@gmail.com>
2025-03-17 23:47:47 +0500

    chore: init

---
+++ src/test/java/com/sabadon/dailycalories/DailycaloriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sabadon.dailycalories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailycaloriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
